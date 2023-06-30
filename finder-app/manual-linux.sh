21e3f44928b816792803b6718f93b203ff967568
Fabio M. De Francesco <fmdefrancesco@gmail.com>
2023-06-30 23:22:44 +0200

    Assignment 3 Part 2
    Signed-off-by: Fabio M. De Francesco <fmdefrancesco@gmail.com>

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -104,8 +104,8 @@ echo "Done Copying dependency files"
 
 # Make device nodes
 
-#sudo mknod -m 666 dev/null c 1 3
-#sudo mknod -m 666 dev/console c 5 1
+sudo mknod dev/null c 1 3
+sudo mknod dev/console c 5 1
 
 # Clean and build the writer utility
 
